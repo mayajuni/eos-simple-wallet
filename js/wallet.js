206fc0203879deb553488c31704c55bbb090f43e
dongjun.kwon <mayajuni10@gmail.com>
2018-08-13 16:45:10 +0900

    전송

--- js/wallet.js
+++ js/wallet.js
@@ -1,12 +1,11 @@
 const config = {
-    httpEndpoint: 'https://api.main-net.eosnodeone.io',
-    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+    httpEndpoint: 'http://dev.cryptolions.io:38888',
+    chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca'
 };
-const eos = Eos(config);
 
 function getInfo() {
     const accountName = document.getElementById('accountName').value;
-    eos.getAccount(accountName).then((account) => {
+    Eos(config).getAccount(accountName).then((account) => {
         document.getElementById('accountBox').style.display = 'block';
         document.getElementById('account').innerHTML = account.account_name;
 
@@ -22,11 +21,25 @@ function getInfo() {
         document.getElementById('total').innerHTML = Number((refund + unstaked + staked).toFixed(4)) + ' EOS';
         document.getElementById('unstake').innerHTML = unstaked + ' EOS';
         document.getElementById('stake').innerHTML = staked + ' EOS';
-        document.getElementById('refund').innerHTML = refund + ' EOS';
+        document.getElementById('refund').innerHTML = refund + ' E/**/OS';
 
         const totalResources = account.total_resources;
         document.getElementById('cpu').innerHTML = `${account.cpu_limit.used}up / ${account.cpu_limit.max}up (${totalResources.cpu_weight})`;
         document.getElementById('net').innerHTML = `${account.net_limit.used}byte / ${account.net_limit.max}byte (${totalResources.net_weight})`;
         document.getElementById('ram').innerHTML = `${account.ram_usage}byte / ${account.ram_quota}byte`;
+    });
+}
+
+function send() {
+    const to = document.getElementById('to').value;
+    const quantity = document.getElementById('quantity').value;
+    const memo = document.getElementById('memo').value;
+    const privateKey = document.getElementById('privateKey').value;
+    const from = document.getElementById('account').innerHTML;
+
+    Eos({...config, keyProvider: [privateKey]}).transaction('eosio.token', (coin) => {
+        coin.transfer(from, to, `${quantity.toFixed(4)} EOS`, memo);
+    }).then(result => {
+        console.log(result);
     });
 }
